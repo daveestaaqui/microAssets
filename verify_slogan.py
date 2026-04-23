2a03ba7ecbcf1ff82eab7d765e4a36d31c28b73a
David Mahler <sandwichfitness@gmail.com>
2026-04-23 16:04:58 -0400

    Cleanup and force push

--- verify_slogan.py
+++
@@ -1,8 +0,0 @@
-import re
-path = "_landing_page/index.html"
-with open(path, "r") as f:
-    content = f.read()
-if "cultivating intelligent workflows" in content:
-    print("FOUND correct slogan in HTML")
-else:
-    print("MISSING correct slogan in HTML")
